aabf5a74625aa26883ee1bf8cf165b7bca20ea33
gabriellychm <gabrielly.chm@gmail.com>
2023-11-14 22:32:02 -0300

    nova seção sobre nós

---
+++ src/pages/SobrePage/SobrePage.tsx
@@ -0,0 +1,15 @@
+import './SobrePage.css'
+import Sobre from '../../components/Sobre/Sobre'
+
+function SobrePage() {
+
+    return (
+      <>
+          <div>
+            <Sobre />
+          </div>
+      </>
+    )
+  }
+  
+  export default SobrePage
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import HomePage from './HomePage/HomePage.tsx'
 import ContatoPage from './ContatoPage/ContatoPage.tsx'
+import SobrePage from './SobrePage/SobrePage.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/contato' element={<ContatoPage />} />
+      <Route path='/sobre' element={<SobrePage />} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
